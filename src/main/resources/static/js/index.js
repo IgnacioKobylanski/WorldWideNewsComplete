6fb34a45bba6e056edb875d2ce4596aa2bdaca10
IgnacioKobylanski <ignaciojk@outlook.com.ar>
2023-11-24 17:40:59 -0300

    the home page is now functional

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,126 +1,29 @@
+import { printHeadlines } from "/js/newsFunction.js";
+import { printNewsByCategory } from "/js/newsFunction.js";
+import { printCarousel } from "/js/newsFunction.js";
+
 document.addEventListener('DOMContentLoaded', function() {
-    // Llama a la función para obtener noticias y llenar el contenido
-    getNewsAndPopulatePage();
+    getNews();
 });
-import {printNews} from "/js/newsFunction.js"
-let sectionHeadline = document.getElementById("headlines");
-fetch('/api/news')
-  .then(response => response.json())
-  .then(newsData => {
 
-    const data = newsData;
+function getNews() {
+    let news;
 
-    console.log(data);
-printNews(data, sectionHeadline)
-  })
-  .catch(error => console.error('Error fetching news:', error));
-
-
-
-
-
-/*
-// Función para obtener noticias desde el backend y mostrarlas
-function getNewsAndPopulatePage() {
-    // Hacer una solicitud al endpoint /api/news
     fetch('/api/news')
         .then(response => response.json())
-        .then(news => {
-            // Llenar el carousel con noticias al azar
-            populateCarousel(news);
+        .then(newsData => {
+            console.log('Noticias obtenidas:', newsData);
 
-            // Llenar la sección "Headlines" con las primeras 3 noticias
-            const headlinesContainer = document.getElementById('headlines-container');
-            const headlinesNews = news.slice(0, 3);
+            news = newsData;
 
-            headlinesNews.forEach(newsItem => {
-                const newsContainer = document.createElement('div');
-                newsContainer.classList.add('small-news');
-
-                // Crear contenido del newsContainer (imagen, título, descripción)
-                newsContainer.innerHTML = `
-                    <img src="${newsItem.imageUrl}" alt="news-img" class="small-news-img">
-                    <div class="text-container">
-                        <h3 class="tile">${newsItem.title}</h3>
-                        <p class="news-description">${newsItem.description}</p>
-                        <a href="/news" class="link-to-news">See more</a>
-                    </div>
-                `;
-
-                // Agregar el newsContainer al contenedor de "Headlines"
-                headlinesContainer.appendChild(newsContainer);
-            });
-
-            // Llenar otras secciones por categoría (si es necesario)
-            populateSection(news, 'Local News', 'local-news');
-            populateSection(news, 'International News', 'international-news');
-            populateSection(news, 'Finance', 'finance');
-            populateSection(news, 'Sports', 'sports');
-            populateSection(news, 'Entertainment', 'entertainment');
+            // Functions
+            printCarousel(news, 'carousel-inner-container')
+            printHeadlines(news);
+            printNewsByCategory(news, 'local-news-container', 'Local News');
+            printNewsByCategory(news, 'international-news-container', 'International News');
+            printNewsByCategory(news, 'finance-container', 'Finance');
+            printNewsByCategory(news, 'sports-container', 'Sports');
+            printNewsByCategory(news, 'entertainment-container', 'Entertainment');
         })
         .catch(error => console.error('Error fetching news:', error));
 }
-
-// Función para llenar el carousel con noticias al azar
-function populateCarousel(news) {
-    // Selecciona el elemento del carousel
-    const carousel = document.querySelector('.carousel-fade');
-
-    const randomNews = news.slice(0, 3);
-
-    // Itera sobre las noticias y crea elementos para el carousel
-    randomNews.forEach((newsItem, index) => {
-        const carouselItem = document.createElement('div');
-        carouselItem.classList.add('carousel-item');
-
-        // La primera noticia se marca como activa
-        if (index === 0) {
-            carouselItem.classList.add('active');
-        }
-
-        // Crear contenido del carouselItem (imagen, título, descripción)
-        carouselItem.innerHTML = `
-            <a href="/news">
-                <img src="${newsItem.imageUrl}" class="d-block w-100" alt="${newsItem.title}">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>${newsItem.title}</h5>
-                    <p>${newsItem.description}</p>
-                </div>
-            </a>
-        `;
-
-        // Agregar el carouselItem al carousel
-        carousel.appendChild(carouselItem);
-    });
-}
-
-// Función para llenar las secciones con noticias por categoría
-function populateSection(news, category, sectionId) {
-    // Filtra las noticias por categoría
-    const categoryNews = news.filter(newsItem => newsItem.category === category);
-
-    // Selecciona el elemento de la sección
-    const section = document.getElementById(sectionId);
-
-    // Obtén las primeras 3 noticias (puedes ajustar según sea necesario)
-    const sectionNews = categoryNews.slice(0, 3);
-
-    // Itera sobre las noticias y crea elementos para la sección
-    sectionNews.forEach(newsItem => {
-        const newsContainer = document.createElement('div');
-        newsContainer.classList.add('small-news');
-
-        // Crear contenido del newsContainer (imagen, título, descripción)
-        newsContainer.innerHTML = `
-            <img src="${newsItem.imageUrl}" alt="news-img" class="small-news-img">
-            <div class="text-container">
-                <h3 class="tile">${newsItem.title}</h3>
-                <p class="news-description">${newsItem.description}</p>
-                <a href="/news" class="link-to-news">See more</a>
-            </div>
-        `;
-
-        // Agregar el newsContainer a la sección
-        section.appendChild(newsContainer);
-    });
-}*/
--- src/main/resources/static/js/newsFunction.js
+++ src/main/resources/static/js/newsFunction.js
@@ -1,12 +1,94 @@
-export function createNews(object) {
-    return `
-        <div class="small-news">
-            <img src="${object.imageUrl}" alt="news-img" class="small-news-img">
+//function for carousell
+export function printCarousel(array, carouselContainerId) {
+    const carouselContainer = document.getElementById(carouselContainerId);
+
+    if (!carouselContainer) {
+        console.error(`Carousel container with id '${carouselContainerId}' not found.`);
+        return;
+    }
+
+    const limit = 3;
+    const limitedArray = array.slice(0, limit);
+
+    for (let index = 0; index < limitedArray.length; index++) {
+        const item = limitedArray[index];
+        const carouselItem = document.createElement("div");
+        carouselItem.classList.add("carousel-item");
+
+        // La primera noticia se marca como activa
+        if (index === 0) {
+            carouselItem.classList.add("active");
+        }
+
+        carouselItem.innerHTML = `
+            <a href="/news">
+                <img src="${item.imageUrl}" class="d-block w-100" alt="${item.title}">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>${item.title}</h5>
+                    <p>${item.description}</p>
+                </div>
+            </a>
+        `;
+
+        // Agregar el carouselItem al contenedor del carrusel
+        carouselContainer.appendChild(carouselItem);
+    }
+}
+
+
+
+
+//function for Headlines
+export function printHeadlines(array) {
+    const headlinesContainer = document.getElementById("headlines-container");
+
+    const limit = 3;
+    const limitedArray = array.slice(0, limit);
+
+    for (let item of limitedArray) {
+        const newsItem = document.createElement("div");
+        newsItem.classList.add("small-news");
+
+
+        newsItem.innerHTML = `
+            <img src="${item.imageUrl}" alt="news-img" class="small-news-img">
             <div class="text-container">
-                <h3 class="tile">${object.title}</h3>
-                <p class="news-description">${object.description}</p>
+                <h3 class="tile">${item.title}</h3>
+                <p class="news-description">${item.description}</p>
                 <a href="/news" class="link-to-news">See more</a>
             </div>
-        </div>
-    `;
+        `;
+
+        headlinesContainer.appendChild(newsItem);
+    }
 }
+
+//Function for the rest of the news
+export function printNewsByCategory(array, containerId, category) {
+    const container = document.getElementById(containerId);
+
+    const categoryNews = array.filter(item => item.category === category);
+
+    for (let item of categoryNews) {
+        const newsItem = document.createElement("div");
+        newsItem.classList.add("small-news");
+
+        newsItem.innerHTML = `
+            <img src="${item.imageUrl}" alt="news-img" class="small-news-img">
+            <div class="text-container">
+                <h3 class="tile">${item.title}</h3>
+                <p class="news-description">${item.description}</p>
+                <a href="/news" class="link-to-news">See more</a>
+            </div>
+        `;
+
+        container.appendChild(newsItem);
+    }
+}
+
+
+
+
+
+
+
